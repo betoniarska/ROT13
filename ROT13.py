62a20aa89a7e6e8748ddf8d416a850df18c95677
betoniarska <115569714+betoniarska@users.noreply.github.com>
2022-12-16 19:56:26 +0200

    Add files via upload

---
+++ ROT13.py
@@ -0,0 +1,36 @@
+
+def salakirjoita(sana):
+
+    kirjaimet = "abcdefghijklmnopqrstuvwxyz"
+    i = 0
+    a = 0
+    uusi = ""
+    while i < len(sana):
+
+        while a < len(kirjaimet):
+            if sana[i] == " ":
+                uusi = uusi + " "
+                break
+            if sana[i] == kirjaimet[a]:  
+                loop = 26 - len(kirjaimet[0:a])
+            
+                if len(kirjaimet[0:a]) + 13 < 26:
+                    uusi = uusi + kirjaimet[len(kirjaimet[0:a]) + 13]
+                    a = 0
+                    break
+                elif len(kirjaimet[0:a]) + 13 >= 26:
+                    uusi = uusi + kirjaimet[a-13]
+                    a = 0
+                    break
+
+            a = a + 1
+        i = i + 1
+    print(uusi)
+
+
+
+
+
+
+
+
